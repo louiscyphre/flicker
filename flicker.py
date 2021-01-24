04fa3875dd2a27b2a566bcc8afa134e64ee0554f
louiscyphre <goldman.sign.and.verify@gmail.com>
2021-01-24 09:20:23 +0200

    Fixed to consume less memory

--- flicker.py
+++ flicker.py
@@ -1,11 +1,17 @@
-# import the necessary packages
 import numpy as np
 import argparse
 import cv2
-import random
+import random as rnd
 import resource
 
-resource.setrlimit(resource.RLIMIT_AS, (6442450944,6442450944))
+resource.setrlimit(resource.RLIMIT_AS, (8589934592,8589934592))
+
+def add_same_frame(queue, frame, minBound=1, maxBound=5):
+	N = rnd.randint(minBound, maxBound)
+	i = 0
+	while i < N:
+		queue.append(frame)
+		i +=1
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
@@ -17,61 +23,57 @@ orig = image.copy()
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 minSize = 0
-maxSize = 15
+maxSize = 3
 thresholdValue = 25
-rounds = 20
-fps = 10
+rounds = 500
+subsetRatio = 40
+fps = 30
 outputFilename = 'tests/sky.mp4'
-
-th, threshed = cv2.threshold(gray, thresholdValue, 255,
-          cv2.THRESH_BINARY)#|cv2.THRESH_OTSU)
-contours = cv2.findContours(threshed, cv2.RETR_LIST,
-                    cv2.CHAIN_APPROX_SIMPLE)[-2]
-
-sized_contours = []
-
-for contour in contours:
-    if minSize < cv2.contourArea(contour) < maxSize:
-        sized_contours.append(contour)
-
-frames = []
-for x in range(rounds):
-		output = orig.copy()
-		not_reversed_frames = []
-		reversed_frames = []
-
-		sizedContoursUpdatedList = random.sample(sized_contours, random.randrange(len(sized_contours)//random.randint(16,32)))
-		for y in sizedContoursUpdatedList:
-			color = list(np.random.random(size=3) * 256)
-			output = cv2.drawContours(output, y, -1, color, 1)
-
-			#N = random.randint(1,3)
-			#i = 0
-			#while i < N:
-			not_reversed_frames.append(output)
-			#	i +=1
-
-		#N = random.randint(1,5)
-		#i = 0
-		#while i < N:
-		#	not_reversed_frames.append(output)
-		#	i +=1
-
-		frames += not_reversed_frames
-		reversed_frames = not_reversed_frames[::-1]
-		frames += reversed_frames
-
-		not_reversed_frames = []
-		reversed_frames = []
-
-		output = None
 
 height, width, layers = orig.shape
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 video = cv2.VideoWriter(outputFilename, fourcc, fps, (width,height))
 
-for frame in frames:
-	video.write(frame)
+th, threshed = cv2.threshold(gray, thresholdValue, 255,
+		  cv2.THRESH_BINARY)#|cv2.THRESH_OTSU)
+contours = cv2.findContours(threshed, cv2.RETR_LIST,
+				    cv2.CHAIN_APPROX_SIMPLE)[-2]
+
+sizedContours = []
+
+for contour in contours:
+	if minSize < cv2.contourArea(contour) < maxSize:
+		sizedContours.append(contour)
+
+frames = []
+for x in range(rounds):
+	output = orig.copy()
+
+	subsetSize = rnd.randrange(len(sizedContours)//
+								   rnd.randint(width//subsetRatio,
+											   (width//subsetRatio)*2))
+	sizedContoursSubset = rnd.sample(sizedContours, subsetSize)
+	print("Subset has: ", {len(sizedContoursSubset)}, " elements")
+	for n in sizedContoursSubset:
+
+		color = list(np.random.random(size=3) * 256)
+
+		outputCopy = output.copy()
+
+		cv2.drawContours(outputCopy, n, -1, color, 1)
+		frames.append(outputCopy)
+
+		output = outputCopy.copy()
+
+	for frame in frames:
+		video.write(frame)
+
+	frames = frames[::-1]
+
+	for frame in frames:
+		video.write(frame)
+
+	frames = []
 
 cv2.destroyAllWindows()
 video.release()
